d6657763adf9b059d81a92c5786b625197c90c2d
Ruben Naranjo <ruben.naranjo@linktic.com>
2025-10-29 15:12:56 -0500

    adjust coverage

--- jest.config.js
+++ jest.config.js
@@ -5,4 +5,8 @@ module.exports = {
   transform: { '^.+\.(t|j)s$': 'ts-jest' },
   moduleNameMapper: { '^src/(.*)$': '<rootDir>/src/$1' },
   testEnvironment: 'node',
+  collectCoverage: true,
+  collectCoverageFrom: ['src/**/*.ts', '!src/main.ts'],
+  coverageDirectory: '<rootDir>/coverage',
+  coverageReporters: ['text', 'lcov', 'cobertura'],
 };
